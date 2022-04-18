707d2bc0fbc1a7eb85505c72d13680ab2c9e6daf
JeEonPark <ggungtoo@icloud.com>
2022-04-19 03:44:06 +0900

    상단바 제거

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'first_page.dart';
 
 import 'package:firebase_core/firebase_core.dart';
@@ -8,6 +9,7 @@ final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
   runApp(
     MaterialApp(
       navigatorKey: navigatorKey,
