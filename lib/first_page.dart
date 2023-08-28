4d1aa5e711d5b40a09dbf7e11bc1b6b0ec522363
JeEon Park <ggungtoo@icloud.com>
2023-08-29 00:51:56 +0900

    const 처리 및 색상 y p 추가

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -20,7 +20,7 @@ class _FirstPageState extends State<FirstPage> {
     //가로 고정
     SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]);
 
-    Color colorDialogue = Colors.blueAccent;
+    // Color colorDialogue = Colors.blueAccent;
     List<Color> colorList = [];
     Color colorD;
 
@@ -29,119 +29,116 @@ class _FirstPageState extends State<FirstPage> {
       home: SafeArea(
         child: Scaffold(
           backgroundColor: Colors.black,
-          body: Container(
-              child: StreamBuilder(
-                  stream: getDataStream(),
-                  builder: (context, AsyncSnapshot streamSnapshot) {
-                    if (streamSnapshot.connectionState == ConnectionState.waiting) {
-                      return Center(child: Text("로딩중...", style: TextStyle(fontSize: 25, color: Colors.white)));
-                    } else {
-                      //Map snapshotList = streamSnapshot.data.docs;
-                      colorList = [];
-                      String colorString = streamSnapshot.data["color"];
-                      colorD = Colors.white;
-                      for (int i = 0; i < 5; i = i + 2) {
-                        //rg
-                        print(colorString.length);
-                        if (i < colorString.length) {
-                          if (colorString[i] == "r") {
-                            colorD = Color.fromARGB(255, 243, 203, 204);
-                          } else if (colorString[i] == "g") {
-                            colorD = Color.fromARGB(255, 167, 200, 157);
-                          } else if (colorString[i] == "b") {
-                            colorD = Color.fromARGB(255, 160, 192, 242);
-                          } else if (colorString[i] == "w") {
-                            colorD = Colors.white;
-                          }
-                        }
-                        colorList.add(colorD);
+          body: StreamBuilder(
+              stream: getDataStream(),
+              builder: (context, AsyncSnapshot streamSnapshot) {
+                if (streamSnapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: Text("로딩중...", style: TextStyle(fontSize: 25, color: Colors.white)));
+                } else {
+                  //Map snapshotList = streamSnapshot.data.docs;
+                  colorList = [];
+                  String colorString = streamSnapshot.data["color"];
+                  colorD = Colors.white;
+                  for (int i = 0; i < 5; i = i + 2) {
+                    //rg
+                    // print(colorString.length);
+                    if (i < colorString.length) {
+                      if (colorString[i] == "r") {
+                        colorD = const Color.fromARGB(255, 243, 203, 204);
+                      } else if (colorString[i] == "g") {
+                        colorD = const Color.fromARGB(255, 167, 200, 157);
+                      } else if (colorString[i] == "b") {
+                        colorD = const Color.fromARGB(255, 160, 192, 242);
+                      } else if (colorString[i] == "w") {
+                        colorD = Colors.white;
+                      } else if (colorString[i] == "y") {
+                        colorD = const Color.fromARGB(255, 255, 228, 0);
+                      } else if (colorString[i] == "p") {
+                        colorD = const Color.fromARGB(255, 255, 190, 216);
                       }
-                      print(colorList);
-                      // ColorString = "rg";
-                      // colorList = [Colors.red, Colors.green, Colors.green];
-                      // String dialogueEx = "올리버)안녕안녕\n클레어)호이호이\n제임스)헬로헬로";
-                      // List dialogueList = dialogueEx.split("\n");
-                      List dialogueList = streamSnapshot.data["dialogue"].split("\n"); // 개수 1~3개
+                    }
+                    colorList.add(colorD);
+                  }
+                  // print(colorList);
+                  // ColorString = "rg";
+                  // colorList = [Colors.red, Colors.green, Colors.green];
+                  // String dialogueEx = "올리버)안녕안녕\n클레어)호이호이\n제임스)헬로헬로";
+                  // List dialogueList = dialogueEx.split("\n");
+                  List dialogueList = streamSnapshot.data["dialogue"].split("\n"); // 개수 1~3개
 
-                      // if (streamSnapshot.data["color"] == "") {
-                      //   colorDialogue = Colors.white;
-                      // } else if (streamSnapshot.data["color"] == "r") {
-                      //   colorDialogue = Color.fromARGB(255, 234, 209, 220);
-                      // } else if (streamSnapshot.data["color"] == "g") {
-                      //   colorDialogue = Color.fromARGB(255, 217, 234, 211);
-                      // } else if (streamSnapshot.data["color"] == "b") {
-                      //   colorDialogue = Color.fromARGB(255, 201, 218, 248);
-                      // }
+                  // if (streamSnapshot.data["color"] == "") {
+                  //   colorDialogue = Colors.white;
+                  // } else if (streamSnapshot.data["color"] == "r") {
+                  //   colorDialogue = Color.fromARGB(255, 234, 209, 220);
+                  // } else if (streamSnapshot.data["color"] == "g") {
+                  //   colorDialogue = Color.fromARGB(255, 217, 234, 211);
+                  // } else if (streamSnapshot.data["color"] == "b") {
+                  //   colorDialogue = Color.fromARGB(255, 201, 218, 248);
+                  // }
 
-                      return Column(
-                        children: [
-                          Spacer(flex: 1),
-                          // 음악 설명(music)
-                          Expanded(
-                              flex: 1,
-                              child: Container(
-                                child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                                  Text(streamSnapshot.data["music"],
-                                      style: TextStyle(
-                                        fontSize: 25,
-                                        color: Colors.white,
-                                        fontFamily: "RIDIBatang",
-                                      ))
-                                ]),
-                              )),
-                          // 소리 설명(effect)
-                          Expanded(
-                              flex: 1,
-                              child: Container(
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text(streamSnapshot.data["effect"],
-                                        style: TextStyle(
-                                          fontSize: 25,
-                                          color: Colors.white,
-                                          fontFamily: "RIDIBatang",
-                                        ))
-                                  ],
-                                ),
-                              )),
-                          // 대사/가사(dialogue)
-                          Expanded(
-                              flex: 3,
-                              child: Container(
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
+                  return Column(
+                    children: [
+                      const Spacer(flex: 1),
+                      // 음악 설명(music)
+                      Expanded(
+                          flex: 1,
+                          child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                            Text(streamSnapshot.data["music"],
+                                style: const TextStyle(
+                                  fontSize: 25,
+                                  color: Colors.white,
+                                  fontFamily: "RIDIBatang",
+                                ))
+                          ])),
+                      // 소리 설명(effect)
+                      Expanded(
+                          flex: 1,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(streamSnapshot.data["effect"],
+                                  style: const TextStyle(
+                                    fontSize: 25,
+                                    color: Colors.white,
+                                    fontFamily: "RIDIBatang",
+                                  ))
+                            ],
+                          )),
+                      // 대사/가사(dialogue)
+                      Expanded(
+                          flex: 3,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Row(children: [
                                     Column(
-                                      mainAxisAlignment: MainAxisAlignment.start,
                                       children: [
-                                        Row(children: [
-                                          Column(
-                                            children: [
-                                              for (int i = 0; i < dialogueList.length; i++)
-                                                (Text(
-                                                  dialogueList[i],
-                                                  style: TextStyle(
-                                                      fontFamily: "RIDIBatang",
-                                                      fontSize: 25,
-                                                      color: colorList[i],
-                                                      height: 1.4,
-                                                      letterSpacing: 1.0),
-                                                  textAlign: TextAlign.center,
-                                                ))
-                                            ],
-                                          )
-                                        ]),
+                                        for (int i = 0; i < dialogueList.length; i++)
+                                          (Text(
+                                            dialogueList[i],
+                                            style: TextStyle(
+                                                fontFamily: "RIDIBatang",
+                                                fontSize: 25,
+                                                color: colorList[i],
+                                                height: 1.4,
+                                                letterSpacing: 1.0),
+                                            textAlign: TextAlign.center,
+                                          ))
                                       ],
                                     )
-                                  ],
-                                ),
-                              )),
-                          Spacer(flex: 1),
-                        ],
-                      );
-                    }
-                  })),
+                                  ]),
+                                ],
+                              )
+                            ],
+                          )),
+                      const Spacer(flex: 1),
+                    ],
+                  );
+                }
+              }),
         ),
       ),
     );
